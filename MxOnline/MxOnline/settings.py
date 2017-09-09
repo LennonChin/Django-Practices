4c62c95a529e0435c0c97641c8b529598f8813fd
LennonChin <i@coderap.com>
2017-09-09 18:12:19 +0800

    modify the UserProfile model, test django admin

--- MxOnline/MxOnline/settings.py
+++ MxOnline/MxOnline/settings.py
@@ -91,6 +91,9 @@ DATABASES = {
         "USER": 'root',
         "PASSWORD": '123456',
         "HOST": '127.0.0.1',
+        'OPTIONS': {
+            "init_command": "SET foreign_key_checks = 0;",
+        }
     }
 }
 
@@ -117,15 +120,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False # seting Fasle used for store data into database use local time
 
 
 # Static files (CSS, JavaScript, Images)
--- MxOnline/apps/users/models.py
+++ MxOnline/apps/users/models.py
@@ -12,8 +12,8 @@ from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
-    birthday = models.DateField(verbose_name=u"生日", default="", blank=True)
-    gender = models.CharField(max_length=5, choices=(("male", u"男"), ("female", u"女")), default="female")
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    gender = models.CharField(max_length=7, choices=(("male", u"男"), ("female", u"女")), default="female")
     address = models.CharField(max_length=100, default=u"")
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
