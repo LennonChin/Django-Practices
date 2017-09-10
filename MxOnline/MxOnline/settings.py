3f87871931b52f89658054c7c3c4e6763d1e93ee
LennonChin <i@coderap.com>
2017-09-10 16:46:45 +0800

    import front website files; config the routes of index and login

--- MxOnline/MxOnline/settings.py
+++ MxOnline/MxOnline/settings.py
@@ -138,3 +138,7 @@ USE_TZ = False # seting Fasle used for store data into database use local time
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
