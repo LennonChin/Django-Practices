fc2ed28f2c5deae7f2a2340547b477fc3ca0c9d8
LennonChin <i@coderap.com>
2017-09-13 23:08:34 +0800

    favorite function with ajax

--- MxOnline/apps/organization/urls.py
+++ MxOnline/apps/organization/urls.py
@@ -3,7 +3,7 @@ __author__ = 'LennonChin'
 __date__ = '2017/09/12 下午 10:23'
 
 from django.conf.urls import url, include
-from views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView
+from views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavoriteView
 
 urlpatterns = [
 
@@ -14,4 +14,7 @@ urlpatterns = [
     url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name="course"),
     url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name="desc"),
     url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name="teacher"),
+
+    # favorite org
+    url(r'^add_fav/$', AddFavoriteView.as_view(), name="add_fav"),
 ]
--- MxOnline/apps/organization/views.py
+++ MxOnline/apps/organization/views.py
@@ -8,9 +8,12 @@ from .models import CourseOrg, CityDict
 
 from .forms import UserAskForm
 
+from operation.models import UserFavorite
+
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -75,13 +78,19 @@ class OrgHomeView(View):
     def get(self, request, org_id):
         current_page = "home"
         course_org = CourseOrg.objects.get(id=int(org_id))
+
+        has_fav = False
+        if request.user.is_authenticated() and UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+            has_fav = True
+
         all_courses = course_org.course_set.all()
         all_teachers = course_org.teacher_set.all()
         context = {
             'current_page': current_page,
             'course_org': course_org,
             'all_courses': all_courses,
-            'all_teachers': all_teachers
+            'all_teachers': all_teachers,
+            'has_fav': has_fav
         }
         return render(request, 'org-detail-homepage.html', context)
 
@@ -90,11 +99,17 @@ class OrgCourseView(View):
     def get(self, request, org_id):
         current_page = "course"
         course_org = CourseOrg.objects.get(id=int(org_id))
+
+        has_fav = False
+        if request.user.is_authenticated() and UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+            has_fav = True
+
         all_courses = course_org.course_set.all()
         context = {
             'current_page': current_page,
             'course_org': course_org,
             'all_courses': all_courses,
+            'has_fav': has_fav
         }
         return render(request, 'org-detail-course.html', context)
 
@@ -103,9 +118,15 @@ class OrgDescView(View):
     def get(self, request, org_id):
         current_page = "desc"
         course_org = CourseOrg.objects.get(id=int(org_id))
+
+        has_fav = False
+        if request.user.is_authenticated() and UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+            has_fav = True
+
         context = {
             'current_page': current_page,
             'course_org': course_org,
+            'has_fav': has_fav
         }
         return render(request, 'org-detail-desc.html', context)
 
@@ -114,10 +135,41 @@ class OrgTeacherView(View):
     def get(self, request, org_id):
         current_page = "teacher"
         course_org = CourseOrg.objects.get(id=int(org_id))
+
+        has_fav = False
+        if request.user.is_authenticated() and UserFavorite.objects.filter(user=request.user, fav_id=course_org.id, fav_type=2):
+            has_fav = True
+
         all_teachers = course_org.teacher_set.all()
         context = {
             'current_page': current_page,
             'course_org': course_org,
             'all_teachers': all_teachers,
+            'has_fav': has_fav
         }
         return render(request, 'org-detail-teachers.html', context)
+
+
+class AddFavoriteView(View):
+    def post(self, request):
+        fav_id = request.POST.get('fav_id', '0')
+        fav_type = request.POST.get('fav_type', '0')
+
+        if not request.user.is_authenticated():
+            return HttpResponse(json.dumps("{'status': 'fail', 'msg': 'not login'}"), content_type='application/json')
+
+        exist_records = UserFavorite.objects.filter(user=request.user, fav_id=int(fav_id), fav_type=int(fav_type))
+
+        if exist_records:
+            exist_records.delete()
+            return HttpResponse(json.dumps("{'status': 'success', 'msg': 'cancel fav'}"), content_type='application/json')
+        else:
+            if int(fav_id) > 0 and int(fav_type) > 0:
+                user_fav = UserFavorite()
+                user_fav.user = request.user
+                user_fav.fav_id = int(fav_id)
+                user_fav.fav_type = int(fav_type)
+                user_fav.save()
+                return HttpResponse(json.dumps("{'status': 'success', 'msg': 'fav success'}"), content_type='application/json')
+            else:
+                return HttpResponse(json.dumps("{'status': 'fail', 'msg': 'fav error'}"), content_type='application/json')
