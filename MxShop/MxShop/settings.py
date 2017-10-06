d8875c04853b9fa2a1fdfb826d522fb41077143d
LennonChin <i@coderap.com>
2017-10-06 16:53:38 +0800

    1, DRF ListAPIView's use; 2, use the pagination;

--- MxShop/MxShop/settings.py
+++ MxShop/MxShop/settings.py
@@ -138,3 +138,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# REST_FRAMEWORK = {
+#     'PAGE_SIZE': 10,
+# }
--- MxShop/apps/goods/views.py
+++ MxShop/apps/goods/views.py
@@ -1,24 +1,23 @@
 from .serializers import GoodsSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Goods
 # Create your views here.
 
 
-class GoodsListView(APIView):
+class GoodsPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    page_query_param = 'p'
+    max_page_size = 100
+
+
+class GoodsListView(generics.ListAPIView):
     """
     商品列表页
     """
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
-
-    def post(self, request, format=None):
-        serializer = GoodsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
