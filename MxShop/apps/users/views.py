2c65164d30021fbaa698277ae6e1f171900a786b
LennonChin <i@coderap.com>
2017-10-19 19:46:46 +0800

    1, completed register function;

--- MxShop/apps/users/views.py
+++ MxShop/apps/users/views.py
@@ -8,6 +8,8 @@ from .serializers import SmsSerializer, UserRegisterSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework_jwt.serializers import jwt_encode_handler, jwt_payload_handler
+
 from utils.yunpian import YunPian
 from MxShop.const import yunpian_apikey
 from .models import VerifyCode
@@ -88,3 +90,19 @@ class UserViewset(CreateModelMixin, viewsets.GenericViewSet):
     """
     serializer_class = UserRegisterSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+
+        result_dict = serializer.data
+        payload = jwt_payload_handler(user)
+        result_dict["token"] = jwt_encode_handler(payload)
+        result_dict["name"] = user.name if user.name else user.username
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(result_dict, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        return serializer.save()
