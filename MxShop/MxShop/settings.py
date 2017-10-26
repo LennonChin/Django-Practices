87169f5ccba43c3a63f1fd3cce7d4fb9dd94cc2b
LennonChin <i@coderap.com>
2017-10-26 23:31:29 +0800

    1, commented sentry config;

--- MxShop/MxShop/settings.py
+++ MxShop/MxShop/settings.py
@@ -211,9 +211,9 @@ SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/index/'
 
 import raven
 
-RAVEN_CONFIG = {
-    'dsn': 'https://<key>:<secret>@sentry.io/<project>',
-    # If you are using git, you can also automatically configure the
-    # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
-}
+# RAVEN_CONFIG = {
+#     'dsn': 'https://<key>:<secret>@sentry.io/<project>',
+#     # If you are using git, you can also automatically configure the
+#     # release based on the git info.
+#     'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+# }
